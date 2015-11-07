e086e620eae4e40bb50e4033abc8588993872ab0
Hean <liuzhihan1988@gmail.com>
2015-11-07 12:20:57 +0800

    spring  aop

---
+++ foundation-web/src/main/java/com/rathink/ie/base/component/CheckOut.java
@@ -0,0 +1,13 @@
+package com.rathink.ie.base.component;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by Hean on 2015/11/5.
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CheckOut {
+}
